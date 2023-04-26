6dabace0686d36e629abd873caa8ee79bec526ab
rukshanarupu <phar.rupu@gmail.com>
2023-04-26 11:20:00 +0600

    name change

--- Module-59/src/Components/Home/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Home = () => {
-    return (
-        <div className='text-red-500 text-center font-bold text-3xl my-4'>This is home</div>
-    );
-};
-
-export default Home;
